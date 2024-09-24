c7e9fceedd8266e4a4aeef717abaa6f8dffb5ef0
Fernando Tozato <101608124+Fernando-Tozato@users.noreply.github.com>
2024-09-24 18:26:25 -0300

    database

---
+++ app/src/main/java/com/example/app_didi_lanches/data/dao/UsuarioDao.kt
@@ -0,0 +1,28 @@
+package com.example.app_didi_lanches.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.app_didi_lanches.data.entity.Usuario
+
+@Dao
+interface UsuarioDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun inserir(usuario: Usuario): Long
+
+    @Query("SELECT * FROM usuarios WHERE id = :id")
+    suspend fun buscarPorId(id: Int): Usuario?
+
+    @Query("SELECT * FROM usuarios")
+    suspend fun listarTodos(): List<Usuario>
+
+    @Update
+    suspend fun atualizar(usuario: Usuario)
+
+    @Delete
+    suspend fun deletar(usuario: Usuario)
+}
