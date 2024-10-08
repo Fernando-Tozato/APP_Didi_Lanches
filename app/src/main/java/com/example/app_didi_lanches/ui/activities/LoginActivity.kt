2e1ada00d0035bc6b99b4741a14caa991caa4c9c
Fernando Tozato <101608124+Fernando-Tozato@users.noreply.github.com>
2024-10-08 20:13:15 -0300

    add activity_main.xml and activity_login.xml

--- app/src/main/java/com/example/app_didi_lanches/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/app_didi_lanches/ui/activities/LoginActivity.kt
@@ -1,2 +1,12 @@
 package com.example.app_didi_lanches.ui.activities
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.app_didi_lanches.R
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
